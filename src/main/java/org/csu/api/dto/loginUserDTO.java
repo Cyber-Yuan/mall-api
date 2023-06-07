36a597e411a266ac09f8d921eef4fa421cba36ec
Shu Yuan <caoshuyuan@xinchong.local>
2023-06-07 18:15:25 +0800

    finnish User part again

--- src/main/java/org/csu/api/dto/loginUserDTO.java
+++ src/main/java/org/csu/api/dto/loginUserDTO.java
@@ -1,12 +1,2 @@
-package org.csu.api.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
-
-@Data
-public class loginDTO {
-    @NotBlank(message = "用户名不能为空")
-    private String username;
-    @NotBlank(message = "密码不能为空")
-    private String password;
+package org.csu.api.dto;public class loginUserDTO {
 }
